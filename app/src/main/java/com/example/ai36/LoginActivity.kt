50ba3e020b9395525396e4ff0cff141db825c38c
sprajapati123 <sandisprajapati52@gmail.com>
2025-05-04 13:12:20 +0545

    prefix

--- app/src/main/java/com/example/ai36/LoginActivity.kt
+++ app/src/main/java/com/example/ai36/LoginActivity.kt
@@ -16,8 +16,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -88,6 +91,12 @@ fun LoginBody(innerPadding: PaddingValues) {
             modifier = Modifier.fillMaxWidth()
                 .padding(horizontal = 10.dp),
             shape = RoundedCornerShape(12.dp),
+            prefix = {
+                Icon(
+                    imageVector = Icons.Default.Person,
+                    contentDescription = null
+                )
+            },
             placeholder = {
                 Text("abc@gmail.com")
             },
