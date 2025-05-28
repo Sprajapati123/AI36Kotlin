d33b4f66190ead51a220896c3add57b4a78df88e
sprajapati123 <sandisprajapati52@gmail.com>
2025-05-28 09:33:13 +0545

    navigation

--- app/src/main/java/com/example/ai36/ListViewActivity.kt
+++ app/src/main/java/com/example/ai36/ListViewActivity.kt
@@ -13,20 +13,27 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -61,6 +68,8 @@ fun ListViewBody() {
         R.drawable.img_6,
     )
 
+    var names = listOf("A","B","C","D","E","F","G","H")
+
     Scaffold { innerPadding ->
         LazyColumn(
             modifier = Modifier
@@ -68,6 +77,23 @@ fun ListViewBody() {
                 .fillMaxWidth()
         ) {
             item {
+
+                LazyVerticalStaggeredGrid(
+                    userScrollEnabled = false,
+                    columns = StaggeredGridCells.Fixed(2),
+                    verticalItemSpacing = 10.dp,
+                    horizontalArrangement = Arrangement.spacedBy(10.dp),
+                    modifier = Modifier.height(1500.dp).fillMaxWidth()
+                ){
+                    items(images.size) { image->
+                        Image(
+                            painter = painterResource(images[image]),
+                            contentDescription = null,
+                            contentScale = ContentScale.Crop,
+                        )
+                    }
+                }
+
                 LazyRow(
                     modifier = Modifier
                         .fillMaxWidth()
@@ -118,32 +144,9 @@ fun ListViewBody() {
                 }
 
 
-            }
-            item {
-                LazyColumn(
-                    modifier = Modifier.height(200.dp),
-                    contentPadding = PaddingValues(10.dp),
-                    verticalArrangement = Arrangement.spacedBy(12.dp)
-                ) {
-                    itemsIndexed(images) { index, image ->
-                        Image(
-                            painter = painterResource(image),
-                            contentDescription = null,
-                            contentScale = ContentScale.Crop,
-                            modifier = Modifier
-                                .height(120.dp)
-                                .border(
-                                    3.dp,
-                                    color = Color.Magenta, shape = CircleShape
-                                )
-                                .fillMaxWidth()
 
-                        )
-                    }
-                }
 
-            }
-            item {
+
                 Box(
                     modifier = Modifier
                         .padding(20.dp)
@@ -187,19 +190,25 @@ fun ListViewBody() {
                     horizontalArrangement = Arrangement.spacedBy(12.dp)
                 ) {
                     itemsIndexed(images) { index, image ->
-                        Image(
-                            painter = painterResource(image),
-                            contentDescription = null,
-                            contentScale = ContentScale.Crop,
-                            modifier = Modifier
-                                .height(100.dp)
-                                .border(
-                                    3.dp,
-                                    color = Color.Magenta, shape = CircleShape
-                                )
-                                .width(100.dp)
-                                .clip(shape = CircleShape)
-                        )
+                        Column (
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ){
+                            Image(
+                                painter = painterResource(image),
+                                contentDescription = null,
+                                contentScale = ContentScale.Crop,
+                                modifier = Modifier
+                                    .height(100.dp)
+                                    .border(
+                                        3.dp,
+                                        color = Color.Magenta, shape = CircleShape
+                                    )
+                                    .width(100.dp)
+                                    .clip(shape = CircleShape)
+                            )
+                            Text(names[index])
+                        }
                     }
                 }
                 Box(
@@ -213,6 +222,7 @@ fun ListViewBody() {
         }
     }
 }
+
 //    val scrollState = rememberScrollState()
 //    val scrollStateRow = rememberScrollState()
 //
