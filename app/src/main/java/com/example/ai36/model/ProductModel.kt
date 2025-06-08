81d77803e47c2bc50a1bd47fa101e66a3c446d33
sprajapati123 <sandisprajapati52@gmail.com>
2025-06-08 13:46:01 +0545

    product repo

--- app/src/main/java/com/example/ai36/model/ProductModel.kt
+++ app/src/main/java/com/example/ai36/model/ProductModel.kt
@@ -4,8 +4,5 @@ data class ProductModel(
     var productId: String = "",
     var productName: String = "",
     var price: Double = 0.0,
-    var quantity: Int = 0,
     var description: String = "",
-    var categoryId: String = "",
-    var isActive: Boolean = false,
 )
---
+++ app/src/main/java/com/example/ai36/repository/ProductRepository.kt
@@ -0,0 +1,39 @@
+package com.example.ai36.repository
+
+import com.example.ai36.model.ProductModel
+
+interface ProductRepository {
+
+    //    {
+//     "success" : true,
+//    "message":"product fetched"
+//    "product":"{ProductModel}"
+//    }
+    fun addProduct(
+        model: ProductModel,
+        callback: (Boolean, String) -> Unit
+    )
+
+    fun getProductById(
+        productId: String,
+        callback: (Boolean, String, ProductModel?) -> Unit
+    )
+
+    fun getAllProduct(
+        callback: (Boolean, String, List<ProductModel?>) -> Unit
+    )
+
+
+    fun updateProduct(
+        productId: String,
+        data: MutableMap<String, Any?>,
+        callback: (Boolean, String) -> Unit
+    )
+
+    fun deleteProduct(
+        productId: String,
+        callback: (Boolean, String) -> Unit
+    )
+
+
+}
