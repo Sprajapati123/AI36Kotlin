cc6f56e86c98b664dc0afc3839b855b036a77d21
sprajapati123 <sandisprajapati52@gmail.com>
2025-06-10 12:07:12 +0545

    vierwmode

---
+++ app/src/main/java/com/example/ai36/viewmodel/ProductViewModel.kt
@@ -0,0 +1,63 @@
+package com.example.ai36.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.ai36.model.ProductModel
+import com.example.ai36.repository.ProductRepository
+
+class ProductViewModel(val repo : ProductRepository) : ViewModel() {
+    fun addProduct(
+        model: ProductModel,
+        callback: (Boolean, String) -> Unit
+    ){
+       repo.addProduct(model,callback)
+    }
+
+    private val _products = MutableLiveData<ProductModel?>()
+    val products : LiveData<ProductModel?> get() = _products
+
+    fun getProductById(
+        productId: String,
+    ){
+        repo.getProductById(productId){
+            success,message,data->
+            if(success){
+                _products.postValue(data)
+            }else{
+                _products.postValue(null)
+
+            }
+        }
+    }
+
+    private val _allProducts = MutableLiveData<List<ProductModel?>>()
+    val allProducts : LiveData<List<ProductModel?>> get() = _allProducts
+
+    fun getAllProduct(){
+        repo.getAllProduct {
+            success,msg,data->
+            if(success){
+                _allProducts.postValue(data)
+            }else{
+                _allProducts.postValue(emptyList())
+
+            }
+        }
+    }
+
+    fun updateProduct(
+        productId: String,
+        data: MutableMap<String, Any?>,
+        callback: (Boolean, String) -> Unit
+    ){
+        repo.updateProduct(productId,data,callback)
+    }
+
+    fun deleteProduct(
+        productId: String,
+        callback: (Boolean, String) -> Unit
+    ){
+     repo.deleteProduct(productId,callback)
+    }
+}
